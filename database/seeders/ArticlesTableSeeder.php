f45b215b78625f2a0da7446be9577ad07878a922
filippovk997 <filippovk997@mail.ru>
2023-03-09 09:44:27 +0400

    state 1

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        $password = Hash::make('admin');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@mail',
+            'password' => $password,
+        ]);
+
+        for ($i = 0; $i < 10; $i++) { 
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
