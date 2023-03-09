ee5da3e0d0b066414ee89d7caafd3cd2723fd6bf
filippovk997 <filippovk997@mail.ru>
2023-03-09 17:48:40 +0400

    clean project from unused auth files

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('articles', [ArticleController::class, 'index']);
     Route::get('articles/{article}', [ArticleController::class, 'show']);
     Route::post('articles', [ArticleController::class, 'store']);
     Route::put('articles/{article}', [ArticleController::class, 'update']);
     Route::delete('articles/{article}', [ArticleController::class, 'delete']);
+    Route::post('logout', [LoginController::class, 'logout']);
 });
 
 Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout']);
 Route::post('register', [RegisterController::class, 'register']);
